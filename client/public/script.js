21eef5d6ad87b23c6635d956fbd13891c03fe7c6
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-28 16:01:38 -0700

    Add HTML elements using Javascript

--- client/public/script.js
+++ client/public/script.js
@@ -1,15 +1,43 @@
 import { products } from '/data.js';
 
 
-const divElement = function (content){
-  return `<div>${content}</div>`;
+const divElement = function(content, firstProperty, secondProperty, thirdProperty) {
+  if (content.details) {
+    return `<div id="${content.id}">
+              ${firstProperty}
+              ${secondProperty}
+              ${thirdProperty}
+            </div>`;
+  } else {
+    return `<div>
+              ${firstProperty}
+              ${secondProperty}
+              ${thirdProperty}
+            </div>`;
+  }
 }
 
 const loadEvent = function() {
 
   // Write your JavaScript code after this line
-  console.log(products[0]);
+  const root = document.getElementById("root");
 
+  products.forEach(album => {
+    const albumName = `<p><b>name: ${album.name}</b></p>`;
+    const albumTaxable = `<b>taxable: ${album.taxable}</b><br>`;
+    const albumStatus = `<i><b>status: ${album.status}</b></i>`;
+    root.insertAdjacentHTML("beforeend", divElement(album, albumName, albumTaxable, albumStatus));
+  });
+
+  products.forEach(album => {
+    const divAlbum = document.getElementById(`${album.id}`);
+    album.details.forEach((track) => {
+      const trackSection = `<section>Name: ${track.name}</section>`;
+      const trackButton = `<button>Unit Price: ${track.unit_price}</button>`;
+      const trackHeader = `<header>Time Span: ${track.milliseconds}</header>`;
+      divAlbum.insertAdjacentHTML("beforeend", divElement(track, trackSection, trackButton, trackHeader));
+    })
+  });
   
   // Write your JavaScript code before this line
 
